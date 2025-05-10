58e6e0827c3c5888459b443aaf69748c3384eeef
k8ie <k8ie@mcld.eu>
2025-05-10 13:17:24 +0200

    Add TODOs

--- quorra/database.py
+++ quorra/database.py
@@ -4,8 +4,10 @@ from fastapi import Depends
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from valkey import Valkey
 
+# TODO: Make DB configurable
 sqlite_url = "sqlite:///database.db"
 engine = create_engine(sqlite_url, echo=True)
+# TODO: Make Valkey configurable
 vk = Valkey(host="localhost", port=6379, db=0, decode_responses=True)
 
 async def get_session():
