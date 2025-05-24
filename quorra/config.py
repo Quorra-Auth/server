ac895736f2e1f236f83d6a850a3ac5d5d42d5dcc
k8ie <k8ie@mcld.eu>
2025-05-24 18:46:11 +0200

    Config expansion

--- quorra/config.py
+++ quorra/config.py
@@ -1,32 +1,39 @@
 import yaml
 from pathlib import Path
 import os
-
+from deepmerge import always_merger
 
 def load_config():
-    CONFIG_PATH = Path(os.getcwd()) / "config.yaml"
+    default_config: dict = {}
+    loaded_config: dict = {}
+    CONFIG_PATH: Path = Path(os.getcwd()) / "config.yaml"
     if "QUORRA_CONFIG" in os.environ:
         CONFIG_PATH = Path(os.environ["QUORRA_CONFIG"])
 
     if CONFIG_PATH.exists():
         try:
             with open(CONFIG_PATH, "r", encoding="utf-8") as config_file:
-                config = yaml.safe_load(config_file)
+                yaml.safe_load(config_file)
         except yaml.YAMLError as exc:
             print("Configuration error: {}".format(exc))
             exit(1)
         else:
             with open(CONFIG_PATH, "r", encoding="utf-8") as config_file:
-                config = yaml.safe_load(config_file)
+                loaded_config = yaml.safe_load(config_file)
     else:
         print("Config file not found!")
         print("Using defaults")
-        config = {"server": {"address": "http://localhost:8080"}, "oidc": {"clients": []}}
+    default_config["server"] = {"address": "http://localhost:8080"}
+    default_config["oidc"] = {"clients": []}
+    default_config["database"] = {}
+    default_config["database"]["sql"] = {"string": "sqlite:///database.db"}
+    default_config["database"]["valkey"] = {"host": "localhost", "port": 6379, "db": 0}
+    always_merger.merge(default_config, loaded_config)
 
     # Default path is None
-    if "path" not in config["server"]:
-        config["server"]["path"] = None
-    return config
+    if "path" not in default_config["server"]:
+        default_config["server"]["path"] = None
+    return default_config
 
 
 def determine_server_url(config: dict) -> str:
--- quorra/database.py
+++ quorra/database.py
@@ -1,14 +1,14 @@
 from typing import Annotated
 from fastapi import Depends
 
+from .config import config
+
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from valkey import Valkey
 
-# TODO: Make DB configurable
-sqlite_url = "sqlite:///database.db"
+sqlite_url = config["database"]["sql"]["string"]
 engine = create_engine(sqlite_url, echo=True)
-# TODO: Make Valkey configurable
-vk = Valkey(host="localhost", port=6379, db=0, decode_responses=True)
+vk = Valkey(host=config["database"]["valkey"]["host"], port=config["database"]["valkey"]["port"], db=config["database"]["valkey"]["db"], decode_responses=True)
 
 async def get_session():
     with Session(engine) as session:
